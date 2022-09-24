89a51630a2dd34ea3123622ea2b2ff23bb435dbf
Brener Pittner <pittner@live.com>
2022-09-24 11:39:32 -0300

    Encontro Remoto 1

---
+++ Chapter/Contexts/ChapterContext.cs
@@ -0,0 +1,24 @@
+﻿using Chapter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter.Contexts
+{
+    public class ChapterContext: DbContext //classe herdada
+    {
+        public ChapterContext() { }
+        public ChapterContext(DbContextOptions<ChapterContext> options) : base(options) { }
+
+        //método para configurar bd
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //sintaxe para o provedor
+                optionsBuilder.UseSqlServer("Data Source = MYMAX\\SQLEXPRESS; initial catalog = Chapter; Integrated Security=true"); //User Id=myUsername;Password=myPassword;
+
+            }
+        }
+
+        public DbSet<Livro> Livros { get; set; }
+    }
+}
